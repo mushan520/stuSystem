b411ae92a8866fade81189b3fda8a989d7b76ec3
mushan520 <55680267+mushan520@users.noreply.github.com>
2020-11-18 15:23:05 +0800

    Add files via upload

---
+++ studentSystem/src/store/index.js
@@ -0,0 +1,57 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import api from '@/api';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    activeStu: null,
+    list: [],
+    size: 3,
+    count: 0,
+    show: false,
+    totalPage: 0,
+    nowPage: 0,
+  },
+  mutations: {
+    setList(state, list) {
+      state.list = list;
+    },
+    setShowModal(state, bool) {
+      state.show = bool;
+    },
+    setActiveStu(state, stu) {
+      state.activeStu = stu;
+    },
+    setTotalPage(state, count) {
+      state.count = count;
+      state.totalPage = Math.ceil(count / state.size);
+    },
+    setNowPage(state, page) {
+      state.nowPage = page;
+    },
+  },
+  actions: {
+    async getStuList({ state, commit }, page) {
+      const { data: { cont: count, findByPage: list } } = await api.findByPage(page, state.size);
+      commit('setList', list);
+      // console.log(list, count, page);
+      commit('setTotalPage', count);
+      commit('setNowPage', page);
+    },
+    async delStu({ state, dispatch }, sNo) {
+      const value = await api.delStu(sNo);
+      console.log(value);
+      let page = Math.ceil((state.count - 1) / state.size);
+      if (state.nowPage > page) {
+        page = state.nowPage - 1;
+      } else {
+        page = state.nowPage;
+      }
+      dispatch('getStuList', page);
+    },
+  },
+  modules: {
+  },
+});
